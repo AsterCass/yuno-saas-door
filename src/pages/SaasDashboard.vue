8e6af755656aac79842041cc76893eedf65e31ae
astercasc <astercass@qq.com>
2024-04-22 10:58:30 +0800

    fix: left drawer in dark model text color error

--- src/pages/SaasDashboard.vue
+++ src/pages/SaasDashboard.vue
@@ -6,15 +6,11 @@
     <DashboardRightMenu/>
 
     <q-page-container>
-      <q-page-container>
-        <q-page padding>
-          <p v-for="n in 20" :key="n">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit nihil praesentium molestias a adipisci,
-            dolore vitae odit, quidem consequatur optio voluptates asperiores pariatur eos numquam rerum delectus
-            commodi perferendis voluptate?
-          </p>
-        </q-page>
-      </q-page-container>
+      <p v-for="n in 50" :key="n">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit nihil praesentium molestias a adipisci,
+        dolore vitae odit, quidem consequatur optio voluptates asperiores pariatur eos numquam rerum delectus
+        commodi perferendis voluptate?
+      </p>
       <router-view/>
     </q-page-container>
 
