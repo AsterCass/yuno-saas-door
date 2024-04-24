1fcfc5812d0cda6a1ed5edd2e50000e89249c9b9
astercasc <astercass@qq.com>
2024-04-24 13:15:10 +0800

    feat: expend left sidebar btn

--- src/components/DashboardLeftMenu.vue
+++ src/components/DashboardLeftMenu.vue
@@ -19,8 +19,8 @@
         <q-separator inset class="q-ma-sm half-opacity"/>
 
         <div class="row items-center q-mt-sm">
-          <q-btn class="full-width no-padding left-switch-drawer-btn-element" flat no-caps
-                 style="border-radius: 1rem">
+          <q-btn class="full-width" flat no-caps
+                 style="border-radius: 1rem; padding: .5rem 0;">
             <template v-slot:default>
               <div class="col row justify-center">
                 <q-avatar size="2.5rem">
@@ -38,26 +38,56 @@
 
         <div class="row items-center q-mt-sm">
 
+          <div class="full-width" v-for="(item, index) in loadUserPageRight" :key="index">
+            <q-btn class=" full-width no-padding" flat no-caps
+                   style="border-radius: 1rem" size="1.2rem" @click="toggleChildShowStatus(item)">
+              <template v-slot:default>
+                <div class="col row justify-center">
+                  <q-avatar size="1.5rem">
+                    <q-icon :name="item.pageIcon"/>
+                  </q-avatar>
+                </div>
+                <h6 v-show="!leftMenuMini" style="text-align: start"
+                    class="no-margin" :class="item.haveChild ? 'col-7': 'col-9'">
+                  {{ item.pageTitle }}
+                </h6>
+                <q-icon v-show="!leftMenuMini && item.haveChild"
+                        class="col-2" size="15px"
+                        :name="item.webIsOpenChild ? 'fa-solid fa-caret-down' : 'fa-solid fa-caret-left'"/>
+              </template>
+            </q-btn>
 
-          <q-btn v-for="(item, index) in loadUserPageRight" :key="index"
-                 class=" full-width no-padding left-switch-drawer-btn-element" flat no-caps
-                 style="border-radius: 1rem" size="1.2rem" @click="toggleChildShowStatus(item)">
-            <template v-slot:default>
-              <div class="col row justify-center">
-                <q-avatar size="1.5rem">
-                  <q-icon :name="item.pageIcon"/>
-                </q-avatar>
+            <transition name="left-switch-drawer-btn-dropdown">
+              <div v-show="item.webIsOpenChild">
+                <div class="full-width" v-for="(item, index) in item.child" :key="index">
+                  <q-btn class=" full-width no-padding" flat no-caps
+                         style="border-radius: 1rem" size="1.2rem" @click="toggleChildShowStatus(item)">
+                    <template v-slot:default>
+                      <div class="col row justify-center">
+                        <q-avatar size="1.5rem">
+                          <q-icon :name="item.pageIcon"/>
+                        </q-avatar>
+                      </div>
+                      <h6 v-show="!leftMenuMini" style="text-align: start"
+                          class="no-margin" :class="item.haveChild ? 'col-7': 'col-9'">
+                        {{ item.pageTitle }}
+                      </h6>
+                      <q-icon v-show="!leftMenuMini && item.haveChild"
+                              class="col-2" size="15px"
+                              :name="item.webIsOpenChild ? 'fa-solid fa-caret-down' : 'fa-solid fa-caret-left'"/>
+                    </template>
+                  </q-btn>
+                </div>
               </div>
-              <h6 v-show="!leftMenuMini" style="text-align: start"
-                  class="no-margin" :class="item.haveChild ? 'col-7': 'col-9'">
-                {{ item.pageTitle }}
-              </h6>
-              <q-icon v-show="!leftMenuMini && item.haveChild"
-                      class="col-2" size="15px"
-                      :name="item.webIsOpenChild ? 'fa-solid fa-caret-down' : 'fa-solid fa-caret-left'"/>
-            </template>
-          </q-btn>
+            </transition>
 
+          </div>
+
+
+        </div>
+
+
+        <div style="min-height: 10rem;background-color: #9C27B0;z-index: 100">
 
         </div>
 
@@ -85,7 +115,7 @@ let loadUserPageRight = ref([
     pageCode: "chart_line",
     pageTitle: "Chart Line",
     pageIcon: "fa-solid fa-chart-line",
-    haveChild: true,
+    haveChild: false,
     webIsOpenChild: false,
     child: []
   },
@@ -93,7 +123,7 @@ let loadUserPageRight = ref([
     pageCode: "shopping_cart",
     pageTitle: "Shopping Cart",
     pageIcon: "shopping_cart",
-    haveChild: true,
+    haveChild: false,
     webIsOpenChild: false,
     child: []
   },
@@ -103,7 +133,48 @@ let loadUserPageRight = ref([
     pageIcon: "navigation",
     haveChild: true,
     webIsOpenChild: false,
-    child: []
+    child: [
+      {
+        pageCode: "chart_line",
+        pageTitle: "Chart Line",
+        pageIcon: "fa-solid fa-chart-line",
+        haveChild: true,
+        webIsOpenChild: false,
+        child: []
+      },
+      {
+        pageCode: "shopping_cart",
+        pageTitle: "Shopping Cart",
+        pageIcon: "shopping_cart",
+        haveChild: true,
+        webIsOpenChild: false,
+        child: []
+      },
+      {
+        pageCode: "navigation",
+        pageTitle: "Navigation",
+        pageIcon: "navigation",
+        haveChild: true,
+        webIsOpenChild: false,
+        child: []
+      },
+      {
+        pageCode: "my_location",
+        pageTitle: "My Location",
+        pageIcon: "my_location",
+        haveChild: false,
+        webIsOpenChild: false,
+        child: []
+      },
+      {
+        pageCode: "edit_location",
+        pageTitle: "Edit Location",
+        pageIcon: "edit_location",
+        haveChild: true,
+        webIsOpenChild: false,
+        child: []
+      },
+    ]
   },
   {
     pageCode: "my_location",
@@ -119,7 +190,46 @@ let loadUserPageRight = ref([
     pageIcon: "edit_location",
     haveChild: true,
     webIsOpenChild: false,
-    child: []
+    child: [{
+      pageCode: "chart_line",
+      pageTitle: "Chart Line",
+      pageIcon: "fa-solid fa-chart-line",
+      haveChild: true,
+      webIsOpenChild: false,
+      child: []
+    },
+      {
+        pageCode: "shopping_cart",
+        pageTitle: "Shopping Cart",
+        pageIcon: "shopping_cart",
+        haveChild: true,
+        webIsOpenChild: false,
+        child: []
+      },
+      {
+        pageCode: "navigation",
+        pageTitle: "Navigation",
+        pageIcon: "navigation",
+        haveChild: true,
+        webIsOpenChild: false,
+        child: []
+      },
+      {
+        pageCode: "my_location",
+        pageTitle: "My Location",
+        pageIcon: "my_location",
+        haveChild: false,
+        webIsOpenChild: false,
+        child: []
+      },
+      {
+        pageCode: "edit_location",
+        pageTitle: "Edit Location",
+        pageIcon: "edit_location",
+        haveChild: true,
+        webIsOpenChild: false,
+        child: []
+      },]
   },
 ])
 
@@ -167,5 +277,21 @@ onUnmounted(() => {
 
 <style scoped lang="scss">
 @import "@/styles/main-style";
+
+.left-switch-drawer-btn-dropdown-enter-active {
+  max-height: 40rem;
+  transition: opacity .8s cubic-bezier(0.1, 0.9, 0.2, 0.9), max-height 0.8s cubic-bezier(0.9, 0.1, 0.9, 0.2);
+}
+
+.left-switch-drawer-btn-dropdown-leave-active {
+  max-height: 40rem;
+  transition: opacity 0.8s cubic-bezier(0.1, 0.9, 0.2, 0.9), max-height 0.8s cubic-bezier(0.1, 0.9, 0.2, 0.9);
+}
+
+.left-switch-drawer-btn-dropdown-enter-from,
+.left-switch-drawer-btn-dropdown-leave-to {
+  opacity: 0;
+  max-height: 0
+}
 
 </style>
