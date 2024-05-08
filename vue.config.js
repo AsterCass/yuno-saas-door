b5e36175bafa4a6ead4f25e06bd1e9025ed2ca1c
astercasc <astercass@qq.com>
2024-05-08 18:18:45 +0800

    feat: update package

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,18 @@
 const {defineConfig} = require('@vue/cli-service')
+const CompressionPlugin = require('compression-webpack-plugin');
+
 module.exports = defineConfig({
+    configureWebpack: {
+        plugins: [
+            new CompressionPlugin({
+                algorithm: 'gzip',
+                test: /\.(js|css|html)$/,
+                threshold: 10240,
+                minRatio: 0.9,
+                deleteOriginalAssets: false,
+            })
+        ]
+    },
     transpileDependencies: [
         'quasar'
     ],
@@ -9,7 +22,16 @@ module.exports = defineConfig({
             rtlSupport: false
         }
     },
-
-    publicPath: process.env.NODE_ENV === 'production' ? '/' : '/'
+    chainWebpack: (config) => {
+        config.plugin('define').tap((definitions) => {
+            Object.assign(definitions[0], {
+                __VUE_OPTIONS_API__: 'true',
+                __VUE_PROD_DEVTOOLS__: 'false',
+                __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: 'false'
+            })
+            return definitions
+        })
+    },
+    publicPath: process.env.NODE_ENV === 'production' ? '/' : '/',
 
 })
